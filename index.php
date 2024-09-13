a6d44a1236287971e813a5b4508e96721456b1c1
Dev_ZIKI <marciojjacometo@gmail.com>
2024-09-13 17:20:03 -0300

    Home com problema consertar mais tarde

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Learn Profit2</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+</head>
+<body>
+    <!-- Menu superior do site a Esquerda(Logo que ao clicar volta para home indiferente da pagina), 
+        meio(páginas de aulas para realizar as atividades e aprender conforme o level do usuário, área de suporte ao usuário),
+        direita(perfil do usuário, aonde ele irá clicar para fazer o logIn ou o cadastro no site) -->
+    <header class="top">
+        <nav class="logo"><a href="index.php"><img src="assets/" alt="Logo Learn Profit"></a></nav>
+        <nav class="options">
+            <div class="aulas">Aulas</div>
+            <div class="suporte">Suporte</div>
+        </nav>
+        <nav class="perfil">
+            <a href=""><img src="" alt="Perfil do usuário"></a>
+        </nav>
+    </header>
+    <!-- Corpo do site, onde ficará todas as informações relevantes presentes nos protótipos -->
+    <section class="mid">
+        
+    </section>
+    <!-- Todas as informações dos Devs presentes aqui(forma de crédito por ajudar a desenvolver o site) -->
+    <footer class="bot">
+
+    </footer>
+
+
+    <script src='main.js'></script>
+</body>
+</html>
